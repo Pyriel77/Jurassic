1fed13d59ccdeae59ce627cf01a42c2ca53e5eeb
Pyriel77 <nyircsak.arpad@gmail.com>
2021-04-29 12:33:40 +0200

    4 tests are green

--- src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
+++ src/main/java/hu/nive/ujratervezes/jurassic/JurassicPark.java
@@ -1,5 +1,9 @@
 package hu.nive.ujratervezes.jurassic;
 
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JurassicPark {
 
     private String dbUrl;
@@ -11,5 +15,25 @@ public class JurassicPark {
         this.dbUser = dbUser;
         this.dbPassword = dbPassword;
     }
+
+    public List<String> checkOverpopulation() {
+        List<String> dinosOverPop = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(dbUrl, dbUser, dbPassword)) {
+            PreparedStatement st = connection.prepareStatement(
+                    "SELECT breed" +
+                            " FROM dinosaur " +
+                            " WHERE actual > expected" +
+                            " ORDER BY breed DESC");
+
+            ResultSet rs = st.executeQuery();
+            while (rs.next()) {
+                dinosOverPop.add(rs.getString("breed"));
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+        return dinosOverPop;
+    }
+
 
 }
